2a8cafafbf2bf32bbacec83b62fc0e6a3bd83c31
Alireza Sheikholmolouki <alireza@miro.com>
2024-04-18 18:01:33 +0200

    feat: installed relevant packages

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello, World!')
+import { EC2Client, DescribeInstancesCommand } from "@aws-sdk/client-ec2";
+
+async function listEC2Instances() {
+  const client = new EC2Client({ region: "eu-west-1" });
+
+  const command = new DescribeInstancesCommand({});
+
+  try {
+    const response = await client.send(command);
+
+    response.Reservations?.forEach((reservation) => {
+      reservation.Instances?.forEach((instance) => {
+        console.log(`Instance ID: ${instance.InstanceId}`);
+        console.log(`Instance Type: ${instance.InstanceType}`);
+        console.log("--------------------");
+      });
+    });
+  } catch (error) {
+    console.error("Error fetching EC2 instances:", error);
+  }
+}
+
+listEC2Instances();
