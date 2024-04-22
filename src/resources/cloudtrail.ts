0c2e311ea57093a4e80ec7460e08826ba630dfd0
Alireza Sheikholmolouki <alireza@miro.com>
2024-04-22 16:40:16 +0200

    feat: added support for cloudtrail

--- src/resources/cloudtrail.ts
+++ src/resources/cloudtrail.ts
@@ -1,7 +1,30 @@
+import { CloudTrailClient, ListTrailsCommand, Trail } from "@aws-sdk/client-cloudtrail";
 import { CloudTrailSchema } from "../types";
 
-export async function getCloudTrailResources(): Promise<CloudTrailSchema> {
-    return {
-        trails: [],
+async function getCloudTrailTrails(): Promise<Trail[]> {
+  const client = new CloudTrailClient({});
+  const trails: Trail[] = [];
+
+  let nextToken: string | undefined;
+  do {
+    const listTrailsCommand = new ListTrailsCommand({
+      NextToken: nextToken,
+    });
+
+    const listTrailsResponse = await client.send(listTrailsCommand);
+
+    if (listTrailsResponse.Trails) {
+      trails.push(...listTrailsResponse.Trails);
     }
+
+    nextToken = listTrailsResponse.NextToken;
+  } while (nextToken);
+
+  return trails;
+}
+
+export async function getCloudTrailResources(): Promise<CloudTrailSchema> {
+  return {
+    trails: await getCloudTrailTrails(),
+  };
 }
