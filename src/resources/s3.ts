f16c996b9c9626c719eacce636d65cc81e4f512b
Alireza Sheikholmolouki <alireza@miro.com>
2024-04-22 16:40:22 +0200

    feat: added support for s3

--- src/resources/s3.ts
+++ src/resources/s3.ts
@@ -1,7 +1,17 @@
+import { S3Client, ListBucketsCommand, Bucket } from "@aws-sdk/client-s3";
 import { S3Schema } from "../types";
 
+async function getS3Buckets(): Promise<Bucket[]> {
+  const client = new S3Client({});
+
+  const listBucketsCommand = new ListBucketsCommand({});
+  const listBucketsResponse = await client.send(listBucketsCommand);
+
+  return listBucketsResponse.Buckets || [];
+}
+
 export async function getS3Resources(): Promise<S3Schema> {
-    return {
-        buckets: [],
-    }
+  return {
+    buckets: await getS3Buckets(),
+  };
 }
