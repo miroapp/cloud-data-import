5311a8b549a12cbf4f85bab0de94f5c0d591684e
Alireza Sheikholmolouki <alireza@miro.com>
2024-05-07 13:11:40 +0200

    feat: added support for s3

--- src/resources/s3.ts
+++ src/resources/s3.ts
@@ -1,25 +1,87 @@
-import { S3Client, ListBucketsCommand, Bucket } from "@aws-sdk/client-s3";
+import { S3Client, ListBucketsCommand, GetBucketLocationCommand, Bucket, ServerSideEncryptionConfiguration, Tag, GetBucketPolicyCommand, GetBucketVersioningCommand, GetBucketEncryptionCommand, GetBucketTaggingCommand } from "@aws-sdk/client-s3";
 import { Resources } from "../types";
+import { buildARN } from "../utils/buildARN";
+import { getAccountId } from "../utils/getAccountId";
 
-// @todo add more details to the bucket object
-async function getS3Buckets(): Promise<Bucket[]> {
-  const client = new S3Client({});
-
-  const listBucketsCommand = new ListBucketsCommand({});
-  const listBucketsResponse = await client.send(listBucketsCommand);
-
-  return listBucketsResponse.Buckets || [];
+interface ExtendedBucket extends Bucket {
+  CreationDate?: Date;
+  LocationConstraint?: string;
+  ARN: string;
+  Policy?: string;
+  Versioning?: string;
+  Encryption?: ServerSideEncryptionConfiguration;
+  Tagging?: Tag[];
 }
 
-export async function getS3Resources(): Promise<Resources<Bucket>> {
+async function getBucketLocation(client: S3Client, bucketName: string): Promise<string> {
+  const command = new GetBucketLocationCommand({ Bucket: bucketName });
+  const response = await client.send(command);
+
+  // Quote from https://docs.aws.amazon.com/AmazonS3/latest/API/API_GetBucketLocation.html#API_GetBucketLocation_ResponseSyntax:
+  // "... Buckets in Region us-east-1 have a LocationConstraint of null."
+  return response.LocationConstraint || 'us-east-1';
+}
+
+async function enrichBucketData(client: S3Client, bucket: Bucket, accountId: string): Promise<ExtendedBucket> {
+  const region = await getBucketLocation(client, bucket.Name!);
+  
+  const arn = buildARN({
+    accountId,
+    region,
+    service: 's3',
+    resource: `bucket/${bucket.Name}`,
+  });
+
+  const [
+    bucketPolicy, 
+    bucketVersioning, 
+    bucketEncryption, 
+    bucketTagging
+  ] = await Promise.all([
+    client.send(new GetBucketPolicyCommand({Bucket: bucket.Name})).catch(() => undefined),
+    client.send(new GetBucketVersioningCommand({Bucket: bucket.Name})).catch(() => undefined),
+    client.send(new GetBucketEncryptionCommand({Bucket: bucket.Name})).catch(() => undefined),
+    client.send(new GetBucketTaggingCommand({Bucket: bucket.Name})).catch(() => undefined)
+  ]);
+
+  return {
+    ...bucket,
+    LocationConstraint: region,
+    ARN: arn,
+    Policy: bucketPolicy?.Policy,
+    Versioning: bucketVersioning?.Status,
+    Encryption: bucketEncryption?.ServerSideEncryptionConfiguration,
+    Tagging: bucketTagging?.TagSet
+  };
+}
+
+async function getS3Buckets(): Promise<ExtendedBucket[]> {
+  const client = new S3Client({});
+  const accountId = await getAccountId();
+
+  try {
+    const listBucketsCommand = new ListBucketsCommand({});
+    const listBucketsResponse = await client.send(listBucketsCommand);
+
+    const enrichedBuckets = await Promise.all(
+      (listBucketsResponse.Buckets || []).map(bucket => {
+        if (!bucket.Name) throw new Error('Bucket Name is missing in the response');
+        return enrichBucketData(client, bucket, accountId);
+      })
+    );
+
+    return enrichedBuckets;
+  } catch (error) {
+    console.error('Error while fetching S3 buckets:', error);
+    throw error;
+  }
+}
+
+export async function getS3Resources(): Promise<Resources<ExtendedBucket>> {
   const buckets = await getS3Buckets();
 
   return buckets.reduce((acc, bucket) => {
-    if (!bucket.Name) {
-      throw new Error('Bucket Name is missing in the response');
-    }
-
-    acc[bucket.Name] = bucket;
+    acc[bucket.ARN] = bucket;
     return acc;
-  }, {} as Resources<Bucket>);
+  }, {} as Resources<ExtendedBucket>);
 }
