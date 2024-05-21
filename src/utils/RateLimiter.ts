3a4bdefd5892195d96317f4f812c31f4ecd38eaf
Alireza Sheikholmolouki <alireza@miro.com>
2024-05-21 16:41:40 +0200

    chore: rate-limiter now supports parallel throttle call

--- src/utils/RateLimiter.ts
+++ src/utils/RateLimiter.ts
@@ -4,53 +4,84 @@ const MAX_RETRIES = 6; // last attempt will take 64 seconds (2^6 = 64 * base ret
 export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 export class RateLimiter {
-    private per: number;
-    private allowance: number;
-    private lastCheck: number;
-    private maxUsage: number;
-  
-    constructor(rate: number, per: number, usagePercentage: number = 0.6) {
-      this.per = per;
-      this.maxUsage = rate * usagePercentage;
-      this.allowance = rate;
-      this.lastCheck = Date.now();
-    }
-  
-    private async acquire() {
-      const current = Date.now();
-      const timePassed = current - this.lastCheck;
-      this.lastCheck = current;
-      this.allowance += (timePassed / this.per) * this.maxUsage;
-  
-      if (this.allowance > this.maxUsage) {
-        this.allowance = this.maxUsage;
-      }
-  
-      if (this.allowance < 1) {
-        const waitTime = (1 - this.allowance) * (this.per / this.maxUsage);
-        await sleep(waitTime);
-        this.allowance = 0;
-      } else {
-        this.allowance -= 1;
-      }
+  private per: number;
+  private allowance: number;
+  private lastCheck: number;
+  private maxUsage: number;
+  private queue: (() => void)[];
+
+  constructor(rate: number, per: number, usagePercentage: number = 0.6) {
+    this.per = per;
+    this.maxUsage = rate * usagePercentage;
+    this.allowance = this.maxUsage;
+    this.lastCheck = Date.now();
+    this.queue = [];
+  }
+
+  private async acquire() {
+    const current = Date.now();
+    const timePassed = current - this.lastCheck;
+    this.lastCheck = current;
+    this.allowance += (timePassed / this.per) * this.maxUsage;
+
+    if (this.allowance > this.maxUsage) {
+      this.allowance = this.maxUsage;
     }
 
-    async throttle<U>(fn: () => Promise<U>): Promise<U> {
-      let attempt = 0;
-      while (attempt < MAX_RETRIES) {
+    if (this.allowance < 1) {
+      const waitTime = (1 - this.allowance) * (this.per / this.maxUsage);
+      await sleep(waitTime);
+      this.allowance = 0;
+    } else {
+      this.allowance -= 1;
+    }
+  }
+
+  async throttle<U>(fn: () => Promise<U>): Promise<U> {
+    return new Promise<U>((resolve, reject) => {
+      const execute = async () => {
         try {
           await this.acquire();
-          return await fn();
+          resolve(await fn());
         } catch (error: any) {
           if (error.name === "ThrottlingException" || error.name === "ProvisionedThroughputExceededException" || error.name === "RequestLimitExceeded") {
-            attempt++;
-            // exponential backoff
-            await sleep(BASE_RETRY_DELAY * Math.pow(2, attempt));
+            this.retry(fn, resolve, reject, 1);
           } else {
-            throw error;
+            reject(error);
           }
         }
+      };
+
+      this.queue.push(execute);
+
+      if (this.queue.length === 1) {
+        this.dequeue();
       }
-      throw new Error("Max retries reached");
+    });
+  }
+
+  private async retry<U>(fn: () => Promise<U>, resolve: (value: U | PromiseLike<U>) => void, reject: (reason?: any) => void, attempt: number) {
+    if (attempt < MAX_RETRIES) {
+      await sleep(BASE_RETRY_DELAY * Math.pow(2, attempt));
+      try {
+        await this.acquire();
+        resolve(await fn());
+      } catch (error: any) {
+        if (error.name === "ThrottlingException" || error.name === "ProvisionedThroughputExceededException" || error.name === "RequestLimitExceeded") {
+          this.retry(fn, resolve, reject, attempt + 1);
+        } else {
+          reject(error);
+        }
+      }
+    } else {
+      reject(new Error("Max retries reached"));
     }
+  }
+
+  private async dequeue() {
+    while (this.queue.length > 0) {
+      const fn = this.queue.shift();
+      if (fn) await fn();
+    }
+  }
 }
