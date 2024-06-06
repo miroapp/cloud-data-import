c8330216e15f15678920dacd26615d9a586034ee
Alireza Sheikholmolouki <alireza@miro.com>
2024-06-06 16:27:27 +0200

    feat: env variables are supported

--- src/app/args.ts
+++ src/app/args.ts
@@ -1,16 +1,25 @@
+import 'dotenv/config'
 import yargs from 'yargs'
 import {hideBin} from 'yargs/helpers'
 import {Config} from '@/types'
 import {awsRegionIds} from '@/constants'
 import {getDefaultOutputName} from './utils/getDefaultOutputName'
 
+function getEnvVar(name: string): string | undefined {
+	return process.env[`CLOUDVIEW_AWS_${name.toUpperCase()}`]
+}
+
 export const config = yargs(hideBin(process.argv))
 	.option('regions', {
 		alias: 'r',
 		type: 'array',
 		description: 'List of regions to scan (use "all" to scan all regions)',
-		demandOption: true,
+		default: getEnvVar('REGIONS')?.split(',') || undefined, // Load from env if set
 		coerce: (arg: string[]) => {
+			if (arg.filter(Boolean).length === 0) {
+				throw new Error('[ERROR] At least one region must be provided')
+			}
+
 			if (arg.includes('all')) {
 				return awsRegionIds
 			}
@@ -18,7 +27,7 @@ export const config = yargs(hideBin(process.argv))
 			const invalidRegions = arg.filter((region) => !awsRegionIds.includes(region))
 			if (invalidRegions.length) {
 				throw new Error(
-					`Invalid region(s): ${invalidRegions.join(', ')}. Valid regions are: ${awsRegionIds.join(', ')}`,
+					`[ERROR] Invalid region(s): ${invalidRegions.join(', ')}. Valid regions are: ${awsRegionIds.join(', ')}`,
 				)
 			}
 
@@ -29,13 +38,13 @@ export const config = yargs(hideBin(process.argv))
 		alias: 'o',
 		type: 'string',
 		description: 'Output file path (must be .json)',
-		default: getDefaultOutputName(),
+		default: getEnvVar('OUTPUT') || getDefaultOutputName(),
 		coerce: (arg: string | string[]) => {
 			if (Array.isArray(arg)) {
 				arg = arg.filter(Boolean)[0] || ''
 			}
 			if (!arg.endsWith('.json')) {
-				throw new Error('Output file must have a .json extension')
+				throw new Error('[ERROR] Output file must have a .json extension')
 			}
 			return arg
 		},
@@ -44,18 +53,24 @@ export const config = yargs(hideBin(process.argv))
 		alias: 'rps',
 		type: 'number',
 		description: 'Maximum number of API calls to make per second (per quota)',
-		default: 10,
+		default: parseFloat(getEnvVar('CALL_RATE_RPS') || '') || 10,
+		coerce: (arg: number) => {
+			if (arg <= 0) {
+				throw new Error('[ERROR] Call rate must be >0')
+			}
+			return arg
+		},
 	})
 	.option('compressed', {
 		alias: 'c',
 		type: 'boolean',
 		description: 'Compress the output',
-		default: false,
+		default: getEnvVar('COMPRESSED') === 'true',
 	})
 	.option('regional-only', {
 		alias: 'ro',
 		type: 'boolean',
 		description: 'Only scan regional services and ignore global services',
-		default: false,
+		default: getEnvVar('REGIONAL_ONLY') === 'true',
 	})
 	.strict().argv as unknown as Config
