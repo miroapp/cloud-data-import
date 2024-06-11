a497a08648fd758d5afa26a8b83b48cc7b28a406
Alireza Sheikholmolouki <alireza@miro.com>
2024-06-11 16:16:45 +0200

    feat: createRateLimiterFactory added

--- src/app/createRateLimiterFactory.ts
+++ src/app/createRateLimiterFactory.ts
@@ -20,7 +20,7 @@ const getQuotaServiceName = (service: string) => {
  *
  * this also handles the shared rate limiters for services like `ec2` and `ec2/volumes` or `rds/clusters` and `rds/db-instances`
  */
-export const getRateLimitService = (config: Config): GetRateLimiterFunction => {
+export const createRateLimiterFactory = (config: Config): GetRateLimiterFunction => {
 	const rateLimiters = new Map<string, RateLimiter>()
 
 	return (service: string, region?: string) => {
