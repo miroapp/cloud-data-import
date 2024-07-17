64f5b9fc8dd6503ae8b5e34aa3be63aaecb4ebe1
Alireza Sheikholmolouki <144152467+alireza-sheikholmolouki@users.noreply.github.com>
2024-07-17 12:29:52 +0200

    Add support for Application Load Balancers (#54)
    * feat: added support for application load balancers in the processing step
    
    * refactor: removed unnecessary null-assertion check
    
    * Create gorgeous-fans-peel.md

--- src/aws-app/process/getProcessedResource.ts
+++ src/aws-app/process/getProcessedResource.ts
@@ -8,77 +8,115 @@ import type * as Redshift from '@aws-sdk/client-redshift'
 
 import {parse} from '@aws-sdk/util-arn-parser'
 
+const getResourceType = (service: string, resource: string): string => {
+	if (service === 'sns') return 'topic'
+	if (service === 'sqs') return 'queue'
+	return resource.split('/')[0].split(':')[0]
+}
+
+const getResourceName = (resourceFullName: string, resourceType: string): string => {
+	if (resourceFullName.startsWith(resourceType)) {
+		// remove the resource type and one more character (usually a colon or a slash)
+		return resourceFullName.slice(resourceType.length + 1)
+	}
+	return resourceFullName
+}
+
 export const getProcessedResource = (arn: string, resource: ResourceDescription): ProcessedResource | null => {
 	const arnData = parse(arn)
 
-	if (!arnData) throw new Error('No arn Data found')
+	if (!arnData) throw new Error('No ARN data found for the provided ARN.')
 
-	const resourceType = (() => {
-		if (arnData.service === 'sns') return 'topic'
-		if (arnData.service === 'sqs') return 'queue'
+	const resourceType = getResourceType(arnData.service, arnData.resource)
+	const name = getResourceName(arnData.resource, resourceType)
 
-		return arnData.resource.split('/')[0].split(':')[0]
-	})()
-
-	const name = (() => {
-		const resourceFullName = arnData.resource // could be either something like "function:name" or "function/name" or in some cases just "name"
-
-		if (resourceFullName.startsWith(resourceType)) {
-			// remove the resource type + one more character (usually a colon or a slash)
-			return resourceFullName.slice(resourceType.length + 1)
-		}
-
-		return resourceFullName
-	})()
-
-	const output: ProcessedResource = {
+	const baseOutput: ProcessedResource = {
 		name,
 		region: arnData.region,
 		type: `${arnData.service}:${resourceType}`,
 	}
 
-	switch (output.type) {
-		case 'lambda:function':
-			output.vpc = (resource as Lambda.FunctionConfiguration).VpcConfig?.VpcId
-			break
-		case 'rds:db':
-			output.vpc = (resource as RDS.DBInstance).DBSubnetGroup?.VpcId
-			output.availabilityZones = (resource as RDS.DBInstance).AvailabilityZone
-				? [String((resource as RDS.DBInstance).AvailabilityZone)]
-				: undefined
-			break
-		case 'rds:cluster':
-			output.availabilityZones = (resource as RDS.DBCluster).AvailabilityZones
-			break
-		case 'ec2:instance':
-			output.vpc = (resource as EC2.Instance).VpcId
-			output.availabilityZones = [(resource as EC2.Instance).Placement?.AvailabilityZone as string]
-			break
-		case 'ec2:volume':
-			output.availabilityZones = [(resource as EC2.Volume).AvailabilityZone as string]
-			break
-		case 'elasticloadbalancing:loadbalancer':
-			output.vpc = (resource as ELBv2.LoadBalancer).VpcId
-			output.availabilityZones = (resource as ELBv2.LoadBalancer).AvailabilityZones?.map(
-				(az) => az.ZoneName || '',
-			).filter(Boolean)
-			break
-		case 'elasticloadbalancing:targetgroup':
-			output.vpc = (resource as ELBv2.TargetGroup).VpcId
-			const targetGroupName = (resource as ELBv2.TargetGroup).TargetGroupName
-			if (targetGroupName) output.name = targetGroupName
-			break
-		case 'elasticfilesystem:file-system':
-			if ((resource as EFS.FileSystemDescription).AvailabilityZoneName) {
-				output.availabilityZones = [(resource as EFS.FileSystemDescription).AvailabilityZoneName as string]
+	switch (baseOutput.type) {
+		case 'lambda:function': {
+			const lambdaResource = resource as Lambda.FunctionConfiguration
+			return {
+				...baseOutput,
+				vpc: lambdaResource.VpcConfig?.VpcId,
 			}
-			break
-		case 'redshift:cluster':
-			output.vpc = (resource as Redshift.Cluster).VpcId
-			output.availabilityZones = [(resource as Redshift.Cluster).AvailabilityZone as string]
-		default:
-			break
-	}
+		}
+		case 'rds:db': {
+			const rdsResource = resource as RDS.DBInstance
+			return {
+				...baseOutput,
+				vpc: rdsResource.DBSubnetGroup?.VpcId,
+				availabilityZones: rdsResource.AvailabilityZone ? [rdsResource.AvailabilityZone] : undefined,
+			}
+		}
+		case 'rds:cluster': {
+			const rdsCluster = resource as RDS.DBCluster
+			return {
+				...baseOutput,
+				availabilityZones: rdsCluster.AvailabilityZones,
+			}
+		}
+		case 'ec2:instance': {
+			const ec2Instance = resource as EC2.Instance
+			return {
+				...baseOutput,
+				vpc: ec2Instance.VpcId,
+				availabilityZones: ec2Instance.Placement?.AvailabilityZone
+					? [ec2Instance.Placement.AvailabilityZone]
+					: undefined,
+			}
+		}
+		case 'ec2:volume': {
+			const ec2Volume = resource as EC2.Volume
+			return {
+				...baseOutput,
+				availabilityZones: ec2Volume.AvailabilityZone ? [ec2Volume.AvailabilityZone] : undefined,
+			}
+		}
+		case 'elasticloadbalancing:loadbalancer': {
+			const elbResource = resource as ELBv2.LoadBalancer
 
-	return output
+			const isALB = elbResource.Type === 'application'
+
+			const type = (() => {
+				if (isALB) return `${baseOutput.type}:application`
+				return baseOutput.type
+			})()
+
+			return {
+				...baseOutput,
+				type,
+				vpc: elbResource.VpcId,
+				availabilityZones: elbResource.AvailabilityZones?.map((az) => az.ZoneName).filter(Boolean) as string[],
+			}
+		}
+		case 'elasticloadbalancing:targetgroup': {
+			const targetGroup = resource as ELBv2.TargetGroup
+			return {
+				...baseOutput,
+				vpc: targetGroup.VpcId,
+				name: targetGroup.TargetGroupName ?? baseOutput.name,
+			}
+		}
+		case 'elasticfilesystem:file-system': {
+			const efsResource = resource as EFS.FileSystemDescription
+			return {
+				...baseOutput,
+				availabilityZones: efsResource.AvailabilityZoneName ? [efsResource.AvailabilityZoneName] : undefined,
+			}
+		}
+		case 'redshift:cluster': {
+			const redshiftCluster = resource as Redshift.Cluster
+			return {
+				...baseOutput,
+				vpc: redshiftCluster.VpcId,
+				availabilityZones: redshiftCluster.AvailabilityZone ? [redshiftCluster.AvailabilityZone] : undefined,
+			}
+		}
+		default:
+			return baseOutput
+	}
 }
