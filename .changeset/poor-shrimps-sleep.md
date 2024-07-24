2b17717901acb56b187b3e5f1cad934fad51b4ee
Alireza Sheikholmolouki <144152467+alireza-sheikholmolouki@users.noreply.github.com>
2024-07-24 13:17:41 +0200

    Exported the scanner and rate-limiter features out for programmatic use (#68)
    * feat: enabling cjs and esm exports
    
    * exported get-scanners and create-rate-limiters out
    
    * added changeset
    
    * fix: mark exports as experimental

---
+++ .changeset/poor-shrimps-sleep.md
@@ -0,0 +1,5 @@
+---
+'@mirohq/cloud-data-import': minor
+---
+
+Exported the scanner and rate-limiter features out, making them available for programmatic use.
---
+++ src/cli.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+// Main entry point
+import discoverAwsApp from './aws-app/main'
+
+const commands = process.argv.slice(2)
+
+async function main() {
+	// If a command is provided and it is not `aws`, print an error message
+	const firstArgIndex = commands.findIndex((arg) => arg.startsWith('-'))
+	const nonArguments = firstArgIndex === -1 ? commands : commands.slice(0, firstArgIndex)
+	if (nonArguments.length > 0 && nonArguments[0] !== 'aws') {
+		throw new Error(`Unknown command: ${nonArguments[0]}`)
+	}
+
+	// If no command is provided, or the command is `aws`, run the AWS app
+	await discoverAwsApp()
+}
+
+main().catch((error) => {
+	console.error(error)
+	process.exit(1)
+})
