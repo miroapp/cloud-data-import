e0a755efabb0af80b6954d507c62b25b40586350
Alireza Sheikholmolouki <alireza@miro.com>
2024-08-08 16:05:17 +0200

    feat: resources are also processed

--- src/aws-app/process/getProcessedData.ts
+++ src/aws-app/process/getProcessedData.ts
@@ -2,14 +2,19 @@ import {ProcessedData, Resources} from '@/types'
 import {getPlacementData} from './getPlacementData'
 import {getProcessedContainers} from './getProcessedContainers'
 import {assignResourcesToContainers} from './assignResourcesToContainers'
+import {getResourcesPlacementData} from './getResourcesPlacementData'
+import {getProcessedResources} from './getProcessedResources'
 
 export const getProcessedData = (resources: Resources): ProcessedData => {
 	const placementData = getPlacementData(resources)
 	const emptyContainers = getProcessedContainers(placementData, resources)
-	const containers = assignResourcesToContainers(placementData, emptyContainers)
+
+	const onlyResourcesPlacementData = getResourcesPlacementData(placementData)
+	const containers = assignResourcesToContainers(onlyResourcesPlacementData, emptyContainers)
+	const processedResources = getProcessedResources(onlyResourcesPlacementData)
 
 	return {
-		resources: {},
+		resources: processedResources,
 		connections: [],
 		containers,
 	}
