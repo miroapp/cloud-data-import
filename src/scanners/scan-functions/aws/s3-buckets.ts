095dcc925dd3452d571e1ed0c7b4f8799a5958e3
Alireza Sheikholmolouki <alireza@miro.com>
2024-10-02 17:13:48 +0200

    feat: s3 is now using both resource-explorer and s3 client

--- src/scanners/scan-functions/aws/s3-buckets.ts
+++ src/scanners/scan-functions/aws/s3-buckets.ts
@@ -1,7 +1,9 @@
 import {S3Client, ListBucketsCommand, Bucket, GetBucketLocationCommand} from '@aws-sdk/client-s3'
 import {Credentials, Resources, RateLimiter, EnrichedBucket} from '@/types'
 import {getAwsAccountId} from './common/getAwsAccountId'
-import {buildARN} from './common/buildArn'
+import {getResourceExplorerClient} from './common/getResourceExplorerClient'
+import {SearchCommand, SearchCommandOutput} from '@aws-sdk/client-resource-explorer-2'
+import {parse as parseArn} from '@aws-sdk/util-arn-parser'
 
 export async function getS3Buckets(
 	credentials: Credentials,
@@ -10,61 +12,127 @@ export async function getS3Buckets(
 	const client = new S3Client({credentials})
 	const accountId = await getAwsAccountId(credentials)
 
-	const buckets = await listBuckets(client, rateLimiter)
-	const enrichedBuckets = await enrichBuckets(buckets, client, rateLimiter)
-
-	return createBucketResources(enrichedBuckets, accountId)
-}
-
-async function listBuckets(client: S3Client, rateLimiter: RateLimiter): Promise<Bucket[]> {
 	const command = new ListBucketsCommand({})
 	const response = await rateLimiter.throttle(() => client.send(command))
-	return response.Buckets ?? []
-}
+	const buckets = response.Buckets ?? []
 
-async function enrichBuckets(buckets: Bucket[], client: S3Client, rateLimiter: RateLimiter): Promise<EnrichedBucket[]> {
-	const enrichPromises = buckets.map((bucket) => enrichBucket(bucket, client, rateLimiter))
-	// Filter out any null results from enrichBucket
-	return (await Promise.all(enrichPromises)).filter((bucket): bucket is EnrichedBucket => !!bucket)
-}
-
-async function enrichBucket(
-	bucket: Bucket,
-	client: S3Client,
-	rateLimiter: RateLimiter,
-): Promise<EnrichedBucket | null> {
-	if (!bucket.Name) return null
-
-	const command = new GetBucketLocationCommand({Bucket: bucket.Name})
-
-	try {
-		const response = await rateLimiter.throttle(() => client.send(command))
-
-		return {
-			Name: bucket.Name,
-			CreationDate: bucket.CreationDate,
-			// Default to 'us-east-1' if LocationConstraint is not provided
-			Location: response.LocationConstraint || 'us-east-1',
+	const enrichedBuckets = await (async () => {
+		try {
+			return await enrichBucketsUsingResourceExplorerClient(buckets, accountId, credentials, rateLimiter)
+		} catch (error) {
+			// If the Resource Explorer is not available, fall back to using the S3 client
+			return await enrichBucketsUsingS3Client(buckets, accountId, client, rateLimiter)
 		}
-	} catch (error) {
-		console.error(`Failed to get ${bucket.Name} bucket location (skipping bucket):`, error)
-		return null
-	}
-}
+	})()
 
-function createBucketResources(buckets: EnrichedBucket[], accountId: string): Resources<EnrichedBucket> {
-	return buckets.reduce((resources, bucket) => {
+	return enrichedBuckets.reduce((resources, bucket) => {
 		if (!bucket.Name) return resources
 
-		const arn = buildARN({
-			service: 's3',
-			region: bucket.Location,
-			accountId,
-			resource: bucket.Name,
-		})
-
+		const arn = `arn:aws:s3:::${bucket.Name}`
 		resources[arn] = bucket
 
 		return resources
 	}, {} as Resources<EnrichedBucket>)
+}
+
+/**
+ * Fetches all S3 buckets using the Resource Explorer.
+ */
+async function enrichBucketsUsingResourceExplorerClient(
+	buckets: Bucket[],
+	accountId: string,
+	credentials: Credentials,
+	rateLimiter: RateLimiter,
+): Promise<EnrichedBucket[]> {
+	const client = await getResourceExplorerClient(credentials)
+	let nextToken: string | undefined = undefined
+
+	const enrichedBuckets: EnrichedBucket[] = []
+
+	do {
+		try {
+			// Search for all S3 resources using the Resource Explorer
+			const command = new SearchCommand({
+				QueryString: 'service:s3',
+				MaxResults: 1000, // The maximum number of results that can be returned in a single call
+				NextToken: nextToken,
+			})
+			const response: SearchCommandOutput = await rateLimiter.throttle(() => client.send(command))
+
+			if (response.Resources) {
+				for (const resource of response.Resources) {
+					if (!resource.Arn) continue
+
+					// Extract the bucket name from the ARN of the resource
+					const resourceExplorerFetchedBucketName = parseArn(resource.Arn).resource
+
+					// Find the bucket in the list of buckets fetched using the S3 client
+					const bucket = buckets.find((bucket) => bucket.Name === resourceExplorerFetchedBucketName)
+
+					if (!bucket) continue
+
+					// If Location is empty, it means the bucket is in us-east-1
+					const location = resource.Region || 'us-east-1'
+
+					// Add the bucket to the list of enriched buckets
+					enrichedBuckets.push({
+						...bucket,
+						Account: accountId,
+						Location: location,
+					})
+				}
+			}
+
+			nextToken = response.NextToken
+		} catch (error) {
+			console.error('Error fetching S3 buckets using Resource Explorer:', error)
+
+			// re-throw the error so caller can try another method
+			throw error
+		}
+	} while (nextToken)
+
+	return enrichedBuckets
+}
+
+/**
+ * Fetches all S3 buckets using the S3 client.
+ */
+async function enrichBucketsUsingS3Client(
+	buckets: Bucket[],
+	accountId: string,
+	client: S3Client,
+	rateLimiter: RateLimiter,
+): Promise<EnrichedBucket[]> {
+	const enrichPromises = buckets.map((bucket) => enrichBucketUsingS3Client(bucket, accountId, client, rateLimiter))
+	// Filter out any null results from enrichBucket
+	return (await Promise.all(enrichPromises)).filter((bucket): bucket is EnrichedBucket => !!bucket)
+}
+
+async function enrichBucketUsingS3Client(
+	bucket: Bucket,
+	accountId: string,
+	client: S3Client,
+	rateLimiter: RateLimiter,
+): Promise<EnrichedBucket | null> {
+	try {
+		const command = new GetBucketLocationCommand({Bucket: bucket.Name})
+		const response = await rateLimiter.throttle(() => client.send(command))
+
+		// If LocationConstraint is empty, it means the bucket is in us-east-1
+		return {
+			...bucket,
+			Account: accountId,
+			Location: response.LocationConstraint || 'us-east-1',
+		}
+	} catch (error) {
+		if (error instanceof Error && 'name' in error && error.name === 'PermanentRedirect') {
+			// Extract the region from the redirect URL
+			const redirectRegion = (error as any).$metadata?.httpHeaders?.['x-amz-bucket-region']
+			if (redirectRegion) {
+				return redirectRegion
+			}
+		}
+		return null
+	}
 }
