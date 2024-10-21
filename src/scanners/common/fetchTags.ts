79b48a3e8149e34c457fe6894f3145a2516c9a20
Andries <andries@miro.com>
2024-10-21 13:04:51 +0200

    Add back chunking

--- src/scanners/common/fetchTags.ts
+++ src/scanners/common/fetchTags.ts
@@ -9,38 +9,48 @@ export const fetchTags = async (
 	const client = new ResourceGroupsTaggingAPIClient({credentials})
 	const tagResult: Record<string, Record<string, string | undefined>> = {}
 
+	const chunkSize = 100 // Set maximum number of ARNs per call
+	const chunks = []
+
+	// Create chunks of ARN list
+	for (let i = 0; i < arnList.length; i += chunkSize) {
+		chunks.push(arnList.slice(i, i + chunkSize))
+	}
+
 	try {
-		let paginationToken: string | undefined = undefined
+		for (const chunk of chunks) {
+			let paginationToken: string | undefined = undefined
 
-		do {
-			const command: GetResourcesCommand = new GetResourcesCommand({
-				PaginationToken: paginationToken,
-				ResourceARNList: arnList,
-			})
+			do {
+				const command: GetResourcesCommand = new GetResourcesCommand({
+					PaginationToken: paginationToken,
+					ResourceARNList: chunk,
+				})
 
-			let response
-			try {
-				response = await rateLimiter.throttle(() => client.send(command))
-			} catch {
-				return {}
-			}
-
-			paginationToken = response.PaginationToken
-
-			for (const resourceData of response.ResourceTagMappingList ?? []) {
-				for (const tagData of resourceData.Tags ?? []) {
-					if (!resourceData.ResourceARN || !tagData.Key) {
-						continue
-					}
-
-					if (!tagResult[resourceData.ResourceARN]) {
-						tagResult[resourceData.ResourceARN] = {}
-					}
-
-					tagResult[resourceData.ResourceARN][tagData.Key] = tagData.Value
+				let response
+				try {
+					response = await rateLimiter.throttle(() => client.send(command))
+				} catch {
+					return {}
 				}
-			}
-		} while (paginationToken)
+
+				paginationToken = response.PaginationToken
+
+				for (const resourceData of response.ResourceTagMappingList ?? []) {
+					for (const tagData of resourceData.Tags ?? []) {
+						if (!resourceData.ResourceARN || !tagData.Key) {
+							continue
+						}
+
+						if (!tagResult[resourceData.ResourceARN]) {
+							tagResult[resourceData.ResourceARN] = {}
+						}
+
+						tagResult[resourceData.ResourceARN][tagData.Key] = tagData.Value
+					}
+				}
+			} while (paginationToken)
+		}
 	} catch (error) {
 		console.error('Error fetching tag resources:', error)
 	}
